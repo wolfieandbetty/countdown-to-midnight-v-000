13c5d0a7f2f8824d842b044c3a7a1195996afca1
Cat Norris <cannenorris@yahoo.com>
2017-11-09 23:55:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,20 @@ def countdown
 "HAPPY NEW YEAR!"
 end
 
+def countdown
+  while number > 0
+    puts "#{n} SECOND(S)!"
+    n-=1
+  end
+"HAPPY NEW YEAR!"
+end
 
-countdown
+def countdown_with_sleep
+  while number > 0
+    puts "#{n} SECOND(S)!"
+    n-=1
+    sleep (1)
+  end
+"HAPPY NEW YEAR!"
+end
+
