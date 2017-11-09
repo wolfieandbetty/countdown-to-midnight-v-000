5e0d341d92022f9ffbbdd65f4072bf56ffbb7492
Cat Norris <cannenorris@yahoo.com>
2017-11-09 23:43:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
-number=10
+n=10
 
 def countdown
   while number=10
-    puts "#{number} SECOND(S)!"
-    number-=1
+    puts "#{n} SECOND(S)!"
+    n-=1
     sleep(1.second)
   end
 end
