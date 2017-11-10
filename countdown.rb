e1bde1158b0797247da0ba51266cc815f0eefc74
Cat Norris <cannenorris@yahoo.com>
2017-11-10 00:01:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 
-def countdown
-  n=10
-  while number > 0
-    puts "#{n} SECOND(S)!"
-    n-=1
+def countdown (seconds_til_midnight)
+  
+  while seconds_til_midnight > 0
+    puts "#{seconds_til_midnight} SECOND(S)!"
+    seconds_til_midnight-=1
   end
 "HAPPY NEW YEAR!"
 end
@@ -19,11 +19,12 @@ def countdown
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  while number > 0
-    puts "#{n} SECOND(S)!"
-    n-=1
-    sleep(5)
+def countdown_with_sleep (seconds_til_midnight)
+  
+  while seconds_til_midnight > 0
+    puts "#{seconds_til_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_til_midnight-=1
   end
 "HAPPY NEW YEAR!"
 end
